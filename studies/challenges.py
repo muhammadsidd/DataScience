56081ff4dd53f89ed5cf9d209f29630fb2a61f47
muhammadsidd <muhammadsidd@mail.usf.edu>
2021-04-15 23:00:31 -0400

    solved challenge 3

--- studies/challenges.py
+++ studies/challenges.py
@@ -18,7 +18,11 @@ def mid(str):
     else:
         k = math.ceil(len(str) / 2)
         return str[k - 1]
+def mid1(string):
+    if len(string) % 2 == 0:
+        return ""
+    return string[len(string)//2]
 
 
-k = mid("aaaa")
+k = mid1("aaaa")
 print(k)
