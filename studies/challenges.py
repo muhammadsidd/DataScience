60234253b53ca5abcec3aa6977f7a90f2ee9d98f
muhammadsidd <muhammadsidd@mail.usf.edu>
2021-04-19 23:06:22 -0400

    apr 19

--- studies/challenges.py
+++ studies/challenges.py
@@ -18,10 +18,10 @@ def mid(str):
     else:
         k = math.ceil(len(str) / 2)
         return str[k - 1]
-def mid1(string):
-    if len(string) % 2 == 0:
-        return ""
-    return string[len(string)//2]
+# def mid1(string):
+#     if len(string) % 2 == 0:
+#         return ""
+#     return string[len(string)//2]
 
 
 k = mid1("acb")
