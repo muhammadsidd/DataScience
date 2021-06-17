67b504941bd5cdcc3c3baba6746c9cd9dd19ca97
muhammadsidd <muhammadsidd@mail.usf.edu>
2021-06-17 18:02:09 -0400

    rad binary write binary

--- studies/fileio.py
+++ studies/fileio.py
@@ -35,5 +35,17 @@ with open('data.txt', 'r') as b:
     b_contents = b.read(20)
     print('cursor after 40 characters here')
     print(b_contents)
+    print(b.tell()) # tells which character we are on in the file at current time
+    b.seek(0) #set file cursor to 0th character and start over you may change seek position to anywhere u like
+
+with open('data2.txt', 'w') as f:
+    f.write('test')
+
+# how to copy pictures using binary read and write 
+# with open('some.jpg','rb') as rf:
+#     with open('some_copy.jpg','wb') as wf:
+#         for line in rf:
+#             wf.write(line)
+
 
     
