63e0a95afaa1025a4774340ef802670399041283
muhammadsidd <muhammadsidd@mail.usf.edu>
2021-06-22 01:30:26 -0400

    final challenge

--- studies/challenges.py
+++ studies/challenges.py
@@ -1,5 +1,5 @@
 import math
-
+######################## Challenge 1########################
 def capital_indexes(cap):
     l1=[]
     for i in range(0,len(cap)):
@@ -11,7 +11,7 @@ def capital_indexes(cap):
             continue
     print(l1)
     return l1
-
+######################## challenge 2###################################
 def mid(str):
     if (len(str) % 2 == 0):
         return ""
@@ -26,7 +26,7 @@ def mid1(string):
 
 k = mid1("acb")
 print(k)
-
+########################## challenge 3##################################
 def online_count(dick):
     count = 0
     for k,v in dick.items():
@@ -41,15 +41,16 @@ statuses = {
 
 from random import randint
 
-
+###################################challenge 4################################
 def random_number():
     return randint(1, 101)
 
 
 print(random_number())
 
-#isinstance(a,int) == true will not work becuz isinstace(True, int) will return True
 
+################### Challenge 5#################################################
+#isinstance(a,int) == true will not work becuz isinstace(True, int) will return True
 def only_ints(a, b):
     if type(a) == int and type(b) == int:
         return True
@@ -58,3 +59,20 @@ def only_ints(a, b):
 
 
 print(only_ints(1, 'a'))
+###################### Final Challenge #####################################
+def format_number(num):
+    if num > 0:
+        vach1 = str(num)[::-1]
+    count = 0
+    l1=[]
+    for i in range (0, len(vach1)):
+        count +=1
+        l1.append(vach1[i])
+        if count == 3 and i != len(vach1)-1:
+            l1.append(',')
+            count = 0
+    e = "".join(l1)
+    return e[::-1]
+
+this = format_number(100000000)
+print (this)
