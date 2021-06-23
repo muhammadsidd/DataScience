0493cab4a9b652bc8986d0d81fe5a47e3ac2dc7b
muhammadsidd <muhammadsidd@mail.usf.edu>
2021-06-22 20:12:42 -0400

    completed

--- studies/algorithms.py
+++ studies/algorithms.py
@@ -9,8 +9,8 @@ class Node:
 class Edge:
 
     def __init__(self, node1, node2, weight):
-        node1.connected[node2.name] = int(weight)
-        node2.connected[node1.name] = int(weight)
+        node1.connected[node2] = int(weight)
+        node2.connected[node1] = int(weight)
 
 a = Node('A')
 b = Node('B')
@@ -52,12 +52,53 @@ e14 = Edge(h, i, 1)
 e15 = Edge(h, j, 3)
 e16 = Edge(i, j, 3)
 
-adj_list = {v: {} for v in node_list}
+# path = {v: float('inf') for v in node_list}
+# path[a] = 0
+# path[b] = -2
+# temporary_nodes = [node for node in node_list]
 
-for node in adj_list:
-    for (k,v) in node.connected.items():
-        adj_list[node][k] = v
+# upper = {node: path[node] for node in temporary_nodes}
+# u = min(upper, key= upper.get)
 
-for k,v in adj_list.items():
-    print("{}:{}".format(k.name, v))
+# for k,v in upper.items():
+#     print(k.name, v)
+
+# print(u.name)
+
+################# Dijkstra Container ###################
+def dijkstra(nodes, start):
+
+    path = {v: float('inf') for v in nodes}
+    path[start] = 0
+    adj_list = {v: {} for v in nodes}
+
+########### Adjecency list ######################
+
+    for node in adj_list:
+        for (k,v) in node.connected.items():
+            adj_list[node][k] = v
+
+    for k,v in adj_list.items():
+        print("{}:{}".format(k.name, v))
+
+    #iterating over a list of nodes each with a value of infinity
+    temporary_nodes = [node for node in nodes]
+
+    while len(temporary_nodes) > 0:
+        #sets a dictionary of all the availabe nodes along with their weight to the current node
+        node_paths = {node: path[node] for node in temporary_nodes}
+        
+        #get the key value of the minimum distance node from the upper list
+        visit = min(node_paths, key= node_paths.get)
+        temporary_nodes.remove(visit)
+
+        for k,v in adj_list[visit].items():
+            path[k] = min(path[k], path[visit]+v)
+    
+    return path
+
+shortestpath = dijkstra(node_list,a)
+
+for k,v in shortestpath.items():
+    print(k.name,v)
 
