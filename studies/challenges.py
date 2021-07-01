934bd0780411f2e881f9ba02b25bcead36294b25
muhammadsidd <muhammadsidd@mail.usf.edu>
2021-06-30 22:22:26 -0400

    small virus

--- studies/challenges.py
+++ studies/challenges.py
@@ -97,3 +97,42 @@ def somefunction(keyFunction, values):
     return dict((keyFunction(v),v) for v in values)
 
 print(somefunction(lambda a: a[0], ["Hello","World"]))
+
+################## delete from ditionary ##################
+
+del d["Oranges"]
+print(d)
+
+############## custom iterator ###################
+
+class MyRange:
+
+    def __init__(self, start, end):
+        self.value = start
+        self.end = end
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.value >= self.end:
+            raise StopIteration
+        current = self.value
+        self.value +=1
+        return current
+
+nums = MyRange(1,10)
+
+for num in nums:
+    print(num)
+
+################### Custom Generators ####################
+
+def my_range (start):
+    current = start
+    while True:
+        yield current
+        current += 1
+nums = my_range(1)
+
+for num in nums:
+    print(num)
