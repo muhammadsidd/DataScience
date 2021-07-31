f255f56b1be1f9f953358bea72c45f8a967e4a69
muhammadsidd <muhammadsidd@mail.usf.edu>
2021-07-30 22:08:11 -0400

    added tester

---
+++ studies/tester.py
@@ -0,0 +1,15 @@
+def solution(N):
+    # write your code in Python 3.6
+    if (N < 10):
+        return 0
+    i = 0
+
+    while N >= 10:
+        N = N // 10
+        i += 1
+    temp = 1
+
+    for j in range(0, i):
+        temp *= 10
+
+    return temp
