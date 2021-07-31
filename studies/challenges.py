960baf077c82fdca9d7a302169147a25ed6e81bf
muhammadsidd <muhammadsidd@mail.usf.edu>
2021-07-30 22:08:43 -0400

    another challenge

--- studies/challenges.py
+++ studies/challenges.py
@@ -136,3 +136,19 @@ nums = my_range(1)
 
 for num in nums:
     print(num)
+
+def solution(N):
+    # write your code in Python 3.6
+    if (N < 10):
+        return 0
+    i = 0
+
+    while N >= 10:
+        N = N // 10
+        i += 1
+    temp = 1
+
+    for j in range(0, i):
+        temp *= 10
+
+    return temp
