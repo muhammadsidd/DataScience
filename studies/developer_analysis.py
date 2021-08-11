4ce8a200ce64369ad5cada90e0b6c16aab5e6fd7
muhammadsidd <muhammadsidd@mail.usf.edu>
2021-08-10 21:34:24 -0400

    renamed columns

--- studies/developer_analysis.py
+++ studies/developer_analysis.py
@@ -1,40 +1,52 @@
 import pandas as pd
 from collections import Counter
 df = pd.read_csv('developerdata.csv', index_col='Respondent')
-print (df.shape)
-print (df.info())
+# print (df.shape)
+# print (df.info())
 
 schema_df = pd.read_csv('developerschema.csv', index_col='Column')
 
 pd.set_option('display.max_columns',85)
 pd.set_option('display.max_rows',85)
 
-print(df.head(5))
-print(schema_df)
+# print(df.head(5))
+# print(schema_df)
 
 ######### to refer to a column in df u can simple use ###
 #### could also use '|' and '&' in the filter of the df. 
-print(df[df['Country'] == 'Pakistan'])
-print(df[df['Country'] == 'Pakistan'].count())
-print("\nTHIS IS COUNTRY\n")
+# print(df[df['Country'] == 'Pakistan'])
+# print(df[df['Country'] == 'Pakistan'].count())
+# print("\nTHIS IS COUNTRY\n")
 
 ########## first record's location #####################
-print(df.loc[1, 'Country'])
+# print(df.loc[1, 'Country'])
 
 ############## value counts gives the count of the columns by adding all the records (rows) ###########
-print(df['Country'].value_counts())
+# print(df['Country'].value_counts())
 
 df2= df['Country'].value_counts()
 
 # df2.to_csv('pakistan.csv')
 
-print("RIP\n")
+# print("RIP\n")
 
-print(df.loc[df['Country']=='Pakistan'].agg(['count']))
+# print(df.loc[df['Country']=='Pakistan'].agg(['count']))
 
-print("RIP\n")
+# print("RIP\n")
 countries = ['United States','Pakistan','United Kingdom','Canada']
 high_sal = ((df['ConvertedComp'] >= 70000) & (df['Country'].isin(countries)) & (df['LanguageWorkedWith'].str.contains('Python', na = False)))
-print(df.loc[high_sal,['Country', 'LanguageWorkedWith']])
+# print(df.loc[high_sal,['Country', 'LanguageWorkedWith']])
 schema_df.sort_index()
-print(schema_df.head(10))
+# print(schema_df.head(10))
+
+# for column in df['Country'].unique():
+#     print(column)
+
+# print([x.upper() for x in df.columns])
+
+df.columns = [x.upper() for x in df.columns]
+
+#will change spaces in column names to _ 
+df.columns = df.columns.str.replace(' ','_')
+
+print(df.head())
