f66c09968b391f6121bb070b6ba2d1882ac74f50
muhammadsidd <muhammadsidd@mail.usf.edu>
2021-08-14 18:44:46 -0400

    some stuff

--- studies/fake_path/threading_ex.py
+++ studies/fake_path/threading_ex.py
@@ -1,22 +1,61 @@
 import threading
-import multiprocessing
+import time
 
-def worker(num):
-    """ Worker procedure
-    """
-    print('Worker:', str(num))
+lock = threading.Lock()
+num_list=[3,9,2,10,6,5,7,4,1,11]
+count = 0 
+pairval = []
 
-jobs = [] # list of jobs
-jobs_num = 5 # number of workers
-for i in range(jobs_num):
-    # Declare a new process and pass arguments to it
-    p1 = multiprocessing.Process(target=worker)
-    jobs.append(p1)
-    # Declare a new process and pass arguments to it
-    p2 = multiprocessing.Process(target=worker)
-    jobs.append(p2)
-    p1.start() # starting workers
-    p2.start() # starting workers
 
-for j in jobs:
-    j.join()
+def test():
+    global num_list
+    global count
+    global pairval
+    
+    while count < len(num_list)-1:
+
+        j = count +1
+
+        while j < len(num_list):
+
+            lock.acquire()
+
+            if num_list[count] + num_list[j] == 12:
+                t =(num_list[count],num_list[j])
+                pairval.append(t)
+
+            lock.release()
+            
+            j = j+1
+        
+        lock.acquire()
+
+        if count<len(num_list):
+            count+=1
+
+        lock.release()
+        
+
+
+thread_list=[]
+start = time.perf_counter()
+
+thread1 = threading.Thread(target=test)
+thread2 = threading.Thread(target=test)
+thread3 = threading.Thread(target=test)
+thread4 = threading.Thread(target=test)
+
+thread1.start()
+thread2.start()
+thread3.start()
+thread4.start()
+
+thread1.join()
+thread2.join()
+thread3.join()
+thread4.join()
+
+end = time.perf_counter()
+
+print(pairval)
+print(end-start)
