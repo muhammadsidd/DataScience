c6ef580f96aa37eb09f563e8e457eace2d6f07ae
muhammadsidd <muhammadsidd@mail.usf.edu>
2021-08-24 20:12:37 -0400

    challanges updated

--- studies/challenges.py
+++ studies/challenges.py
@@ -152,3 +152,20 @@ def solution(N):
         temp *= 10
 
     return temp
+
+## palandrome that takes care of one offset 
+
+def pal(str, str2):
+    temp = str[::-1]
+    count = 0 
+
+    for i in range(len(str2)):
+        if temp[0] != str2[0]:
+            count +=1
+        
+    if temp == str2 or count <= 1:
+        return True 
+    else:
+        return False
+
+print(pal('stats', 'foo'))
---
+++ studies/romcom.py
@@ -0,0 +1,25 @@
+import requests
+import json
+import pandas as pd
+import numpy as np
+from pprint import pprint
+import random
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import unicodedata
+import re
+
+url = "https://en.wikipedia.org/wiki/List_of_romantic_comedy_films"
+
+tables = pd.read_html(url)
+romcom_df = pd.concat(tables,sort=False)
+print(tables[1])
+
+# 
+romcom_df.columns = ["Year","Title","Director","Country","Notes","Empty", "NA", "NA2"]
+romcom_df = romcom_df.filter(items=["Year","Title","Director","Country"])
+romcom_df = romcom_df.set_index("Country")
+romcom_df = romcom_df.filter(like='United States', axis= 0)
+romcom_df = romcom_df.reset_index()
+romcom_df = romcom_df.filter(items=["Year","Title"])
+romcom_df.to_csv('romcom2.csv')
